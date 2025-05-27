ce90f894850ef9899c8ccb15047f07f31ce474dd
Franck DAKIA <dakiafranck@gmail.com>
2025-05-27 23:33:15 +0000

    fix: by add trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->web([
             \App\Http\Middleware\Localization::class,
         ]);
