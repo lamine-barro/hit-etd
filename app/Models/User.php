102548cead807c7c9e88581f3b438dc20f773ce8
Franck DAKIA <dakiafranck@gmail.com>
2025-06-09 22:13:30 +0000

    fix: user access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\HasName;
+use Filament\Models\Contracts\HasAvatar;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasAvatar, HasName
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +47,39 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'otp_expires_at' => 'datetime',
         ];
+    }
+
+    /**
+     * Determine if the user can access the Filament panel.
+     *
+     * @param \Filament\Panel $panel
+     * @return bool
+     */
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        // Allow all users to access the panel by default
+        return true;
+    }
+
+    /**
+     * Get the URL to the user's avatar for Filament.
+     *
+     * @return string|null
+     */
+    public function getFilamentAvatarUrl(): ?string
+    {
+        // Return null or a default avatar URL
+        return null;
+    }
+
+    /**
+     * Get the display name for the user in Filament.
+     *
+     * @return string
+     */
+    public function getFilamentName(): string
+    {
+        // Return the user's email as their name, or customize as needed
+        return $this->name;
     }
 }
