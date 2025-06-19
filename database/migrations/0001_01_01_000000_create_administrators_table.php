db6dabaa294b9d9b74f983f381e57e3f36a7773d
Franck DAKIA <dakiafranck@gmail.com>
2025-06-19 16:52:11 +0000

    fix: migration

--- database/migrations/0001_01_01_000000_create_administrators_table.php
+++ database/migrations/0001_01_01_000000_create_administrators_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('administrators', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id('id')->primary();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('avatar_url')->nullable();
